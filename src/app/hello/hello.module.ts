655ce16faa45fee97e178ec79c27978f12894b70
Jesper Haapalinna <jesper.haapalinna@eatech.fi>
2018-10-22 21:52:23 +0300

    Edit Hello Componet routing. Edit Hello Module to import SharedModule

--- src/app/hello/hello.module.ts
+++ src/app/hello/hello.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HelloComponent } from './hello.component';
 import { HelloRoutingModule } from './hello-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    HelloRoutingModule
+    HelloRoutingModule,
+    SharedModule
   ],
   declarations: [HelloComponent]
 })
